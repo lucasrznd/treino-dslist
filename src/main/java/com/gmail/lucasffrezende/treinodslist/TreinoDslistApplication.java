c137813207caab41400ff26047af9a4874ee9efe
Lucas Rezende <lucasffrezende@gmail.com>
2023-05-12 16:34:15 -0300

    Project created

---
+++ src/main/java/com/gmail/lucasffrezende/treinodslist/TreinoDslistApplication.java
@@ -0,0 +1,13 @@
+package com.gmail.lucasffrezende.treinodslist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreinoDslistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TreinoDslistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gmail/lucasffrezende/treinodslist/TreinoDslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gmail.lucasffrezende.treinodslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreinoDslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
